7d5f7e9c66133308d1a593b616987277eaee905f
tedwujiasheng <wjs5253569@163.com>
2019-05-30 15:37:32 +0800

    master

--- helloworld2/src/helloworld2/hello.java
+++ helloworld2/src/helloworld2/hello.java
@@ -4,5 +4,6 @@ public class hello {
 	public static void main(String[] args) {
 		System.out.println("hello world 2");
 		System.out.println("update 2");
+		System.out.println("master update");
 	}
 }
